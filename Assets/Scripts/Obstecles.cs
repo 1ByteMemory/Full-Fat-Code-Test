21754f18c72942cb6b64a3adb53bcf3de0c41755
Charlotte <32960450+1ByteMemory@users.noreply.github.com>
2021-11-06 18:34:59 +0000

    Obstacles spawn inside the lanes

--- Assets/Scripts/Obstecles.cs
+++ Assets/Scripts/Obstecles.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Obstecles : MonoBehaviour
 {
 
-	public float playerToObstacleSpawnDistance = 30.0f;
+	public float ObstacleSpawnDistance = 15;
 	public float minSpawnTime = 0.5f;
 	public float maxSpawnTime = 3.0f;
 
 	public Transform[] obsteclesList;
+
+	int[,] spawningSpots;
 
 	Transform player;
 	PlayerController pc;
@@ -21,9 +23,11 @@ public class Obstecles : MonoBehaviour
 		
 		pc = player.GetComponent<PlayerController>();
 
+		spawningSpots = new int[pc.numberOfLanes, Mathf.FloorToInt(GetComponent<GroundReplacer>().groundZSize)];
+
 		for (int i = 0; i < obsteclesList.Length; i++)
 		{
-			SpawnObstecle(obsteclesList[i], playerToObstacleSpawnDistance + Random.Range(i + 5.0f, i + 15.0f));
+			SpawnObject(obsteclesList[i], 10 + i * 4);
 		}
     }
 
@@ -32,17 +36,48 @@ public class Obstecles : MonoBehaviour
     {
         
     }
+	
 
-	void SpawnObstecle(Transform obstecle, float distanceFromPlayer)
+	void SpawnObject(Transform objectToSpawn, float distanceFromPlayer)
 	{
 		
-		obstecle.position = new Vector3(
-			Random.Range(0, pc.numberOfLanes),					// Randomize its lane position
+		Debug.Log("Object: " + objectToSpawn);
+		objectToSpawn.position = new Vector3(
+			RandLane(pc.numberOfLanes),					// Randomize its lane position
 			0,
-			player.position.z + distanceFromPlayer	// Set position infront of player
+			Mathf.Floor(player.position.z + distanceFromPlayer)	// Set position infront of player, floored to make it more uniform
 		);
-
 		
+	}
+
+	int RandYpos(int spaces)
+	{
+		
+		return 0;
+	}
+
+	/// <summary>
+	/// Returns the x position of a random lane
+	/// </summary>
+	/// <param name="laneAmount">The total number of lanes</param>
+	/// <returns></returns>
+	int RandLane(int laneAmount)
+	{
+		int randomLane = Random.Range(1, laneAmount + 1) * 2 - 2;
+		int offset = Mathf.FloorToInt(laneAmount / 2) * 2;
+
+		return randomLane - offset;
+	}
+
+	/// <summary>
+	/// Returns the x position of a given lane
+	/// </summary>
+	/// <param name="laneAmount"></param>
+	/// <param name="laneNumber"></param>
+	/// <returns></returns>
+	int LaneToXPos(int laneAmount, int laneNumber)
+	{
+		return (laneNumber * 2) - (Mathf.FloorToInt(laneAmount / 2) * 2);
 	}
 
 }
